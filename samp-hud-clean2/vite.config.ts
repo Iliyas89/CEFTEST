0519b92be87e7247152cc08cda775b281234cf57
Iliyas89 <toqehiliyas@gmail.com>
2026-06-10 15:41:06 +0000

    test

--- samp-hud-clean2/vite.config.ts
+++ samp-hud-clean2/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',   //base: '/CEFTEST/', // <-- Должно быть ИМЕННО CEFTEST, как в ссылке
+  base: '/CEFTEST/', // <-- Должно быть ИМЕННО CEFTEST, как в ссылке
 })
